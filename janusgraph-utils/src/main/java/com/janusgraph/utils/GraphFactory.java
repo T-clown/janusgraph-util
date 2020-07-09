6393127f89efcd4fafcb80b4c782f8e4aa56ba18
YCKJ2717 <tianjiangnan@cloudwalk.cn>
2020-07-09 17:20:38 +0800

    初始化

--- janusgraph-utils/src/main/java/com/janusgraph/utils/GraphFactory.java
+++ janusgraph-utils/src/main/java/com/janusgraph/utils/GraphFactory.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
