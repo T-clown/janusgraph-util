c3c28c84dcadaf54edeb4d2315b5db4fc272a54c
YCKJ2717 <tianjiangnan@cloudwalk.cn>
2020-07-10 17:01:01 +0800

    dev

--- janusgraph-util/src/main/java/com/janusgraph/utils/GremlinUtils.java
+++ janusgraph-util/src/main/java/com/janusgraph/utils/GremlinUtils.java
@@ -2,8 +2,11 @@ package com.janusgraph.utils;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.concurrent.ThreadPoolExecutor;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
@@ -16,12 +19,23 @@ public class GremlinUtils {
     @Autowired
     ThreadPoolExecutor threadPoolExecutor;
 
-    public void addVertices(GraphTraversalSource g, String label, List<Map<String, Object>> vertices) {
-        GraphTraversal<Vertex, Vertex> traversal = g.addV();
-        for (Map<String, Object> vertex : vertices) {
-            traversal.addV(label);
-            vertex.forEach((k, v) -> traversal.property(k, v));
+    public static void addVertices(GraphTraversalSource g, String label, List<Map<String, Object>> vertices) {
+        if (CollectionUtils.isNotEmpty(vertices)) {
+            GraphTraversal<Vertex, Vertex> traversal = null;
+            for (Map<String, Object> vertex : vertices) {
+                if (traversal == null) {
+                    traversal = g.addV(label);
+                } else {
+                    traversal.addV(label);
+                }
+                Set<Entry<String, Object>> entries = vertex.entrySet();
+                for (Entry<String, Object> entry : entries) {
+                    traversal.property(entry.getKey(), entry.getValue());
+                }
+            }
+            if (traversal != null) {
+                traversal.next();
+            }
         }
-        traversal.next();
     }
 }
