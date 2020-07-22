4c351a6c56d6f9d7504d6e2d26bebf797c0161a2
YCKJ2717 <tianjiangnan@cloudwalk.cn>
2020-07-22 11:51:17 +0800

    dev

--- janusgraph-util/src/main/java/com/janusgraph/utils/GremlinUtils.java
+++ janusgraph-util/src/main/java/com/janusgraph/utils/GremlinUtils.java
@@ -6,13 +6,19 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ThreadPoolExecutor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.janusgraph.core.schema.JanusGraphManagement;
+import org.janusgraph.core.schema.SchemaAction;
+import org.janusgraph.graphdb.database.management.GraphIndexStatusReport;
+import org.janusgraph.graphdb.database.management.ManagementSystem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class GremlinUtils {
 
@@ -20,22 +26,31 @@ public class GremlinUtils {
     ThreadPoolExecutor threadPoolExecutor;
 
     public static void addVertices(GraphTraversalSource g, String label, List<Map<String, Object>> vertices) {
-        if (CollectionUtils.isNotEmpty(vertices)) {
-            GraphTraversal<Vertex, Vertex> traversal = null;
-            for (Map<String, Object> vertex : vertices) {
-                if (traversal == null) {
-                    traversal = g.addV(label);
-                } else {
-                    traversal.addV(label);
-                }
-                Set<Entry<String, Object>> entries = vertex.entrySet();
-                for (Entry<String, Object> entry : entries) {
-                    traversal.property(entry.getKey(), entry.getValue());
-                }
-            }
-            if (traversal != null) {
-                traversal.next();
+        if (CollectionUtils.isEmpty(vertices)) {
+            return;
+        }
+        GraphTraversal<Vertex, Vertex> traversal = g.addV(label);
+        vertices.get(0).forEach((k, v) -> traversal.property(k, v));
+        if (vertices.size() > 1) {
+            for (int i = 1; i < vertices.size(); i++) {
+                traversal.addV(label);
+                vertices.get(i).forEach((k, v) -> traversal.property(k, v));
             }
         }
+        traversal.next();
+    }
+
+    public static boolean updateIndexStatus(String index, SchemaAction action) {
+        try (GraphFactory factory = new GraphFactory()) {
+            JanusGraphManagement mgmt = factory.getGraph().openManagement();
+            mgmt.updateIndex(mgmt.getGraphIndex(index), action).get();
+            mgmt.commit();
+            GraphIndexStatusReport call = ManagementSystem.awaitGraphIndexStatus(factory.getGraph(), index).call();
+            //call.getConvergedKeys();
+            return call.getSucceeded();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return false;
     }
 }
